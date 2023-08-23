c0d5d62c3cbb27a71119fbc4eb36a1b9a04438a2
madanadra <mlaksindra@gmail.com>
2023-08-23 08:03:48 +0700

    final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 
   return (
     <html lang="en">
-      <meta name="description" content="Games by Muhammad Laksmana Indra" />
+      <meta name="description" content="InGames by Muhammad Laksmana Indra" />
       <body className='min-h-screen font-pixel text-blue-700'>
         <audio ref={backgroundMusic} src='./background-music.mp3' />
         <img src='./background-image.gif' alt='Background' className='fixed w-full h-full object-cover -z-50' />
